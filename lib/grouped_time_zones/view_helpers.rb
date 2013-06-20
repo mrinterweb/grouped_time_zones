925e32a1b2e4d3b67fdd3ae09aa400f71a15361f
Jeremy Ward <jward@crowdcompass.com>
2013-06-20 14:33:28 -0700

    Added ability to force a unique list of timezones based on the tzinfo.indentifier attribute.
    Basically, this prevents multiple time zone values from appearing as select options. Example: "Pacific/Midway" would only appear once.

--- lib/grouped_time_zones/view_helpers.rb
+++ lib/grouped_time_zones/view_helpers.rb
@@ -1,17 +1,23 @@
 module GroupedTimeZones
   module ViewHelpers
-    def  grouped_time_zone_select(name, attribute, record=nil)
-
-      select_tag("#{name}[#{attribute}]", grouped_options_for_select(grouped_time_zones, (record ? record.send(attribute.to_sym) : nil)))
+    def  grouped_time_zone_select(name, attribute, record=nil, force_uniq=false)
+      select_tag("#{name}[#{attribute}]", grouped_options_for_select(grouped_time_zones(force_uniq), (record ? record.send(attribute.to_sym) : nil)))
     end
 
-    def grouped_time_zones
-      us_zones = ActiveSupport::TimeZone.us_zones
-      other_zones = ActiveSupport::TimeZone.all.reject{|tz| tz.name =~ %r{/} }.sort - us_zones
-      zone_options = lambda do |zones|
+    def grouped_time_zones(force_uniq=false)
+      us_zones      = ActiveSupport::TimeZone.us_zones
+      non_us_zones  = other_zones(us_zones, force_uniq)
+      zone_options  = lambda do |zones|
         zones.map { |tz| [tz.to_s, tz.tzinfo.identifier] }
       end
-      return [['United States', zone_options.call(us_zones)], ['Other', zone_options.call(other_zones)]]
+      return [['United States', zone_options.call(us_zones)], ['Other', zone_options.call(non_us_zones)]]
+    end
+
+    private
+
+    def other_zones(excluded_zones, force_uniq=false)
+      other_zones = force_uniq ? ActiveSupport::TimeZone.all.reject{|tz| tz.name =~ %r{/} }.uniq{|tz| tz.tzinfo.identifier } : ActiveSupport::TimeZone.all.reject{|tz| tz.name =~ %r{/} }
+      other_zones.sort - excluded_zones
     end
   end
 end
