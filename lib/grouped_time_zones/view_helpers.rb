ae676d6eadf5a011c8e6a3ff2481254595cbfb5a
Jeremy Ward <jward@crowdcompass.com>
2013-06-20 13:26:31 -0700

    Fixed broken spec.
    ActiveSupport added 2 new timezones which broke the current specs.

--- lib/grouped_time_zones/view_helpers.rb
+++ lib/grouped_time_zones/view_helpers.rb
@@ -1,7 +1,7 @@
 module GroupedTimeZones
   module ViewHelpers
     def  grouped_time_zone_select(name, attribute, record=nil)
-      
+
       select_tag("#{name}[#{attribute}]", grouped_options_for_select(grouped_time_zones, (record ? record.send(attribute.to_sym) : nil)))
     end
 
